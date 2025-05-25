55e10decc4795c1793746978c0b7bb2cc51214db
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-05-26 00:18:01 +0200

    Add footer, favicon and css

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p><a href="/about">Om siden</a></p>
+        <p><a href="https://github.com/Vebb02/f1">GitHub</a></p>
+        <p><a href="/contact">Kontakt</a></p>
+        <p><a href="/privacy">Personvernerklæring</a></p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
