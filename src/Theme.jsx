aa8806fa75d45590123fe2f119386c44f63df146
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-05-28 10:47:43 +0200

    Make sure theme is set before initial render

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -2,20 +2,8 @@ import { useEffect, createContext, useState } from "react";
 
 const ThemeContext = createContext();
 
-const getTheme = () => {
-	const theme = localStorage.getItem("theme");
-	if (!theme) {
-		const isUsingLightTheme = window.matchMedia("(prefers-color-scheme: light)");
-		const preferredTheme = isUsingLightTheme.matches ? "light" : "dark";
-		localStorage.setItem("theme", preferredTheme);
-		return preferredTheme;
-	} else {
-		return theme;
-	}
-};
-
 const ThemeProvider = ({ children }) => {
-	const [theme, setTheme] = useState(getTheme);
+	const [theme, setTheme] = useState(localStorage.getItem("theme"));
 	const themes = ["light", "dark", "oled"];
 	function toggleTheme() {
 		const themeIndex = (themes.indexOf(theme) + 1) % themes.length;
