0acbb842bf7c810b3388125b92f881de9c7e350b
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-05-28 12:36:42 +0200

    Add proxy for login

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
+      '/oauth2/authorization/google': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
     },
   },
 })
