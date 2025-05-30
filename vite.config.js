c7cb2db79257b46b9f555107aeb4cb68419711de
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-05-30 15:30:14 +0200

    Remove proxy

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-      '/logout': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-      '/oauth2/authorization/google': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-    },
-  },
 })
