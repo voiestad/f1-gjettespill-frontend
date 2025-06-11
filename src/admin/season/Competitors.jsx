10bf50b41c264760983c48521d67aeedc6e6fda5
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-06-11 18:58:09 +0200

    Add page for season contructors

--- src/admin/season/Competitors.jsx
+++ src/admin/season/Competitors.jsx
@@ -17,7 +17,187 @@ export function SeasonCompetitors() {
 }
 
 export function SeasonConstructors() {
+  const { year } = useParams();
+  const [constructors, setConstructors] = useState(null);
+  const [positions, setPositions] = useState(null);
+  const [newConstructor, setNewConstructor] = useState("");
+  function loadConstructors() {
+    axios.get(`/api/admin/season/competitors/constructors/list/${year}`)
+      .then(res => {
+        setConstructors(res.data);
+        setPositions(res.data.map(_ => 1));
+      })
+      .catch(err => console.error(err));
+  }
 
+  useEffect(() => {
+    loadConstructors();
+  }, []);
+
+  function changePos(event, constructor, newPosition) {
+    event.preventDefault();
+    axios.get('/api/public/csrf-token')
+      .then(res => {
+        const headerName = res.data.headerName;
+        const token = res.data.token;
+        axios.post('/api/admin/season/competitors/constructors/move', {},
+          {
+            params: {
+              year: year,
+              constructor: constructor,
+              newPosition: newPosition
+            },
+            headers: {
+              [headerName]: token
+            }
+          })
+          .then(res => {
+            loadConstructors();
+          })
+          .catch(err => {
+            alert('Kunne ikke endre plass');
+            console.error(err);
+          })
+      })
+      .catch(err => console.error(err));
+  }
+
+  function changeColor(event, constructor) {
+    event.preventDefault();
+    const color = new FormData(event.target).get('color');
+    if (!color) {
+      return;
+    }
+    axios.get('/api/public/csrf-token')
+      .then(res => {
+        const headerName = res.data.headerName;
+        const token = res.data.token;
+        axios.post('/api/admin/season/competitors/constructors/add-color', {},
+          {
+            params: {
+              year: year,
+              constructor: constructor,
+              color: color
+            },
+            headers: {
+              [headerName]: token
+            }
+          })
+          .then(res => {
+            loadConstructors();
+          })
+          .catch(err => {
+            alert('Kunne ikke endre farge');
+            console.error(err);
+          })
+      })
+      .catch(err => console.error(err));
+  }
+
+  function deleteConstructor(event, constructor) {
+    event.preventDefault();
+    if (!confirm("Er du sikker på at du ville slette konstruktøren?")) {
+      return;
+    }
+    axios.get('/api/public/csrf-token')
+      .then(res => {
+        const headerName = res.data.headerName;
+        const token = res.data.token;
+        axios.post('/api/admin/season/competitors/constructors/delete', {},
+          {
+            params: {
+              year: year,
+              constructor: constructor
+            },
+            headers: {
+              [headerName]: token
+            }
+          })
+          .then(res => {
+            loadConstructors();
+          })
+          .catch(err => {
+            alert('Kunne ikke slette konstruktør');
+            console.error(err);
+          })
+      })
+      .catch(err => console.error(err));
+  }
+
+  function updatePos(index, value) {
+    setPositions(positions.map((pos, i) =>
+      i === index ? value : pos
+    ))
+  }
+
+  function addConstructor(event) {
+    event.preventDefault();
+    if (!newConstructor) {
+      return;
+    }
+    axios.get('/api/public/csrf-token')
+      .then(res => {
+        const headerName = res.data.headerName;
+        const token = res.data.token;
+        axios.post('/api/admin/season/competitors/constructors/add', {},
+          {
+            params: {
+              year: year,
+              constructor: newConstructor
+            },
+            headers: {
+              [headerName]: token
+            }
+          })
+          .then(res => {
+            loadConstructors();
+            setNewConstructor("");
+          })
+          .catch(err => {
+            alert('Kunne ikke legge til konstruktør');
+            console.error(err);
+          })
+      })
+      .catch(err => console.error(err));
+  }
+
+  return (
+    <>
+      <h2>Konstruktører {year}</h2>
+      <form>
+        <input type="text" placeholder="Konstruktørnavn" value={newConstructor}
+          onChange={e => setNewConstructor(e.target.value)} />
+        <input type="submit" value="Legg til konstruktør" onClick={addConstructor} />
+      </form>
+      {constructors ?
+        constructors.map((constructor, index) =>
+          <div key={constructor.competitor}>
+            <p>{index + 1}. {constructor.competitor}</p>
+            <form>
+              {positions[index]}<br />
+              <input type="range" min="1" max={constructors.length} value={positions[index]}
+                onChange={e => updatePos(index, e.target.value)} />
+              <br />
+              <input type="submit" value="Endre plass"
+                onClick={e => changePos(e, constructor.competitor, positions[index])} />
+            </form>
+            <br />
+            <form onSubmit={e => changeColor(e, constructor.competitor)}>
+              {constructor.color ?
+              <span className="circle" style={{backgroundColor: constructor.color}} />
+              : ''}
+              <input type="text" pattern="^#[0-9A-Fa-f]{6}$" placeholder="#ffffff" name="color" defaultValue={constructor.color ? constructor.color : ''} />
+              <input type="submit" value="Velg farge" />
+            </form>
+            <form>
+              <input type="submit" value="&#128465;"
+                onClick={e => deleteConstructor(e, constructor.competitor)} />
+            </form>
+          </div>
+        )
+        : ''}
+    </>
+  )
 }
 
 export function SeasonDrivers() {
