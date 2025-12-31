89503010a6a5ec753ab093eb6a85193e37eb6aa5
Vebjørn Øiestad <102312685+voiestad@users.noreply.github.com>
2025-12-31 12:06:34 +0100

    Add form for changing league name

--- src/league/LeagueSettings.jsx
+++ src/league/LeagueSettings.jsx
@@ -1,16 +1,53 @@
 import axios from 'axios';
 import { useState, useEffect, useContext } from 'react';
-import { useParams, Link, useNavigate } from 'react-router';
+import { useParams, useNavigate } from 'react-router';
 import { CsrfTokenContext } from '../components';
 import { ErrorNotMember, ErrorUnknown } from '../error';
-import Table from '../util/Table';
 
 function LeagueSettings() {
   const [membership, setMembership] = useState(null);
   const [error, setError] = useState(null);
   const { leagueId } = useParams();
+  const [leagueName, setLeagueName] = useState("");
+  const [message, setMessage] = useState(null);
   const navigate = useNavigate();
   const { token, headerName } = useContext(CsrfTokenContext);
+
+  function validateLeagueName(e) {
+    let value = e.target.value;
+    let result = "";
+    for (const c of value) {
+      if (result.length == 50) {
+        break;
+      }
+      if (c.match("^[a-zA-ZÆØÅæøå0-9 ]$")) {
+        result += c;
+      }
+    }
+    setLeagueName(result);
+  }
+
+  function createLeague(e) {
+    e.preventDefault();
+    if (!leagueName) {
+      return;
+    }
+    axios.post("/api/league/rename", {}, {
+      params: {
+        leagueName: leagueName,
+        leagueId: leagueId,
+      },
+      headers: {
+        [headerName]: token
+      }
+    }).then(res => {
+      setLeagueName("");
+      setMessage("Nytt navn ble satt!");
+    }).catch(err => {
+      console.error(err)
+      setMessage(err.response.data);
+    })
+  }
 
   useEffect(() => {
     axios.get(`/api/public/league/membership/${leagueId}`)
@@ -74,6 +111,12 @@ function LeagueSettings() {
             {membership === "OWNER" ?
               <>
                 <input type="submit" onClick={deleteLeague} value="Slett liga" />
+
+                <form>
+                  <input type="text" placeholder="Liganavn" onChange={validateLeagueName} value={leagueName} /><br />
+                  <input type="submit" value="Endre liganavn" onClick={createLeague} />
+                </form>
+                {message ? <p>{message}</p> : ''}
               </> : ''}
           </> :
           <>
